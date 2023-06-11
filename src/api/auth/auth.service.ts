2f8585d3aeea7759327e9a43423190532464aed9
aasif-sysonex <aasif@sysonex.dev>
2023-06-11 16:03:01 +0600

    updated session time

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -74,7 +74,7 @@ export class AuthService {
     const secret = this.config.get('JWT_SECRET');
 
     const token = await this.jwt.signAsync(payload, {
-      expiresIn: '90d',
+      expiresIn: '30m',
       secret: secret
     });
 
